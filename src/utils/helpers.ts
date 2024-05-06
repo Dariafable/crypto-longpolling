cccdd8e11fc4a435381c94d1981d104ba9a7ed82
Daria Nebylitsa <daranebylitsa@gmail.com>
2024-05-06 21:45:20 +0200

    fixed helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { TArrayData } from "../types";
+
 export const getMinValue = (data: TArrayData): number => {
   return Number(Math.min(...Object.values(data).flat()).toFixed(2));
 };
@@ -6,7 +7,3 @@ export const getMinValue = (data: TArrayData): number => {
 export const formatNumber = (currencyValue: number): number => {
   return Number(currencyValue.toFixed(2));
 };
-
-/* export const formatValues = (values: number[]): number[] => {
-  return values.map((value) => Number(value.toFixed(2)));
-}; */
